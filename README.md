b17362c82bc1350538fb57d646eafdaadae02347
silverelizard <corrine.olson@careerbuilder.com>
2015-04-13 11:04:06 +0200

    Working twitter service

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# twitter
+# PencilBlue Twitter Plugin
 Twitter widget for PencilBlue CMS. Uses the Twitter RESTful API endpoint `/get/statuses/user_timeline`. Documentation for this endpoint can be found in  [Twitter's Developer Documentation](https://dev.twitter.com/rest/reference/get/statuses/user_timeline).
 
 In order to use the plugin you must have a Twitter account and setup an application. The plugin settings require `consumer_key`, `consumer_secret`, `access_token_key`, and `access_token_secret` from an app. There is also a fully customizable settings page for all of the parameters defined for the api endpoint. Either `user_id` or `screen_name` is required for the API to function.
+
+The main use case for the plugin is setting up the parameters and then calling the TwitterService in a template in order to gain access to the tweet data for display. For example, the following code snipper checks if the plugin is installed and then accesses the service:
+
+```JavaScript
+if(pb.PluginService.isActivePlugin('twitter')) { //checks if the twitter plugin is installed
+  var TwitterService = pb.PluginService.getService('twitterService', 'twitter'); //gets the twitter service from the plugin
+  var twitterService = new TwitterService(); //creates a new instance of the twitter service
+  twitterService.getTweets(function(tweets) { //calls the getTweets method from the twitter service with a callback
+    pb.log.info(tweets); //logs tweets from service to console
+  });
+}
+```
--- services/twitter_service.js
+++ services/twitter_service.js
@@ -12,20 +12,47 @@ module.exports = function TwitterServiceModule(pb) {
     return "twitterService";
   };
 
-  TwitterService.prototype.getStream = function(cb){
-    var client = setupTwitterClient();
-    
+  TwitterService.prototype.getTweets = function(cb){
+    getParameters(function(parameters) {
+      getClientInfo(function(clientInfo) {
+        var client = new Twitter(clientInfo);
+        client.get('statuses/user_timeline', parameters, function(error, tweets, response){
+          if(error) self.reqHandler.serveError(error);
+          cb(tweets);
+        });
+      });
+    });
   };
   
-  function setupTwitterClient() {
+  function getClientInfo(cb) {
     var pluginService = new pb.PluginService();
     var clientInfo = {};
     pluginService.getSettings('twitter', function(err, twitterSettings) {
       twitterSettings.forEach(function(setting) {
         clientInfo[setting.name] = setting.value;
       });
+      cb(clientInfo);
     });
-    return new Twitter(clientInfo);
+  }
+  
+  function getParameters(cb) {
+    var parameters = {};
+    var dao = new pb.DAO();
+    var opts = {
+      where: {settings_type: 'api_parameter'}
+    };
+    dao.q('twitter_plugin_settings', opts, function(err, settings) {
+      if (util.isError(err)) {
+        self.reqHandler.serveError(err);
+      }
+      if(settings.length > 0) {
+        settings = settings[0];
+        settings.parameters.forEach(function(parameter) {
+          parameters[parameter.type] = parameter.value;
+        });
+      }
+      cb(parameters);
+    });
   }
 
   return TwitterService;
