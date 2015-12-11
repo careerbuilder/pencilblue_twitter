f96aff82297e7dde42ef20d5b0a37a99f69eb2a3
wesanderson303 <wes.anderson@careerbuilder.com>
2015-12-11 13:21:47 -0500

    Twitter settings clean up on uninstall

--- twitter.js
+++ twitter.js
@@ -6,9 +6,29 @@ module.exports = function TwitterModule(pb){
     cb(null, true);
   };
 
-  Twitter.onUninstall = function(cb) {
-    cb(null, true);
-  };
+	/**
+	 * Called when the application is uninstalling this plugin.  The plugin should
+	 * make every effort to clean up any plugin-specific DB items or any in function
+	 * overrides it makes.
+	 *
+	 * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
+	 * The result should be TRUE on success and FALSE on failure
+	 */
+	Twitter.onUninstallWithContext = function (context, cb) {
+		var site = pb.SiteService.getCurrentSite(context.site);
+
+		var queryService = new pb.SiteQueryService({site: site, onlyThisSite: true});
+		pb.log.silly('Preparing to delete twitter plugin settings');
+		queryService.delete({site: site}, 'twitter_plugin_settings', function (err, commandResult) {
+			if (err || !commandResult) {
+				pb.log.error("Error: Failed to remove twitter plugin settings for site " + site);
+				return cb(null, true);
+			}
+			pb.log.silly("Successfully deleted twitter plugin settings for site " + site);
+			pb.log.info("Twitter Plugin Uninstalled");
+			cb(null, true);
+		});
+	};
 
   Twitter.onStartup = function(cb) {
     pb.AdminSubnavService.registerFor('plugin_settings', function(navKey, localization, data) {
